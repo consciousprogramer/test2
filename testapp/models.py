3d204e258f403fb8a490fd9260ddcb706cb41297
consciousprogramer <rohitkumar9133@gmail.com>
2020-06-21 18:47:39 +0530

    added __str__ method in master

--- testapp/models.py
+++ testapp/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class student(models.Model):
     name = models.CharField(max_length = 100)
     age = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
